fd580a2151ab1a84149800a60a9b3a0f4fe84826
Annaldas Rahul <rahulannaldas2006@gmail.com>
2025-12-15 22:57:42 +0530

    update code

---
+++ day2.py
@@ -0,0 +1,8 @@
+#Check if an Array is Sorted 
+
+'''Example 1:
+Input: N = 5, array[] = {1,2,3,4,5}
+Output: True.
+Explanation: The given array is sorted i.e Every element in the array is smaller than or equals to its next values, So the answer is True.
+'''
+
