67cae051b6fde964fe030be957aa12b65ea08892
Annaldas Rahul <rahulannaldas2006@gmail.com>
2026-01-19 22:32:41 +0530

    Medium level

--- Arrays/day8.py
+++ Arrays/day8.py
@@ -1,1 +1,17 @@
-#revision
+#Two Sum : Check if a pair with given sum exists in Array
+#Problem Statement: Given an array of integers arr[] and an integer target.
+#1st variant: Return YES if there exist two numbers such that their sum is equal to the target. Otherwise, return NO.
+#2nd variant: Return indices of the two numbers such that their sum is equal to the target. Otherwise, we will return {-1, -1}.
+
+#This is the logic for the problem should be prepared
+
+
+'''def Twosum(arr,target):
+    for i in range(0,len(arr)):
+        need=target-arr[i]
+        if need in arr:
+            return True
+    return False
+
+print(Twosum([2,6,5,11],14))'''
+
