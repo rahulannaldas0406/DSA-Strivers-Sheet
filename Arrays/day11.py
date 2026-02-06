606bbaeb4be5e878a867df1a8a1ed85db359a663
Annaldas Rahul <rahulannaldas2006@gmail.com>
2026-02-06 23:38:44 +0530

    Medium level

--- Arrays/day11.py
+++ Arrays/day11.py
@@ -2,4 +2,44 @@
 # Problem Statement: Given an N * N 2D integer matrix, rotate the matrix by 90 degrees clockwise. 
 # The rotation must be done in place, meaning the input 2D matrix must be modified directly..
 
+'''This is brute force '''
+
+
+'''arr=[[1,2,3],[4,5,6],[7,8,9]]
+n=len(arr)
+row=len(arr)
+col=len(arr)
+matrix=[]
+for i in range(row):
+    rows = []
+    for j in range(col):
+        rows.append(0)
+    matrix.append(rows)
+print(len(arr))
+print(len(matrix))
+
+for i in range(len(arr)):
+    for j in range(len(arr)):
+        matrix[j][n-1-i]=arr[i][j]
+
+for row in matrix:
+    print(*row)'''
+
+
+'''Better approach '''
+
+
+'''arr=[[1,2,3],[4,5,6],[7,8,9]]
+
+for i in range(len(arr)):
+    for j in range(len(arr)):
+        if i<j:
+            arr[i][j],arr[j][i]=arr[j][i],arr[i][j]
+
+for row in arr:
+    row.reverse()
+
+
+for row in arr:
+    print(*row)'''
 
