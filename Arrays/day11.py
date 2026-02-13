3890eca4e70fe4a493e1dda44aa76e7b12fa40a6
Annaldas Rahul <rahulannaldas2006@gmail.com>
2026-02-13 22:02:31 +0530

    Medium problems

--- Arrays/day11.py
+++ Arrays/day11.py
@@ -29,7 +29,7 @@ for row in matrix:
 '''Better approach '''
 
 
-'''arr=[[1,2,3],[4,5,6],[7,8,9]]
+arr=[[1,2,3],[4,5,6],[7,8,9]]
 
 for i in range(len(arr)):
     for j in range(len(arr)):
@@ -41,5 +41,5 @@ for row in arr:
 
 
 for row in arr:
-    print(*row)'''
+    print(*row)
 
