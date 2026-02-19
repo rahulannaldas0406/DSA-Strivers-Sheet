952817b3cb289a0c8bf0a398afb0057b3fbf6ee1
Annaldas Rahul <rahulannaldas2006@gmail.com>
2026-02-19 23:46:34 +0530

    Update code

--- Arrays/day6.py
+++ Arrays/day6.py
@@ -22,7 +22,7 @@ Distnict Elements in arr1 are : 1,6,7,8,9,10
 Distnict Elemennts in arr2 are : 11,12
 Union of arr1 and arr2 is {1,2,3,4,5,6,7,8,9,10,11,12}'''
 
-'''arr1=[1,2,3,4]
+arr1=[1,2]
 arr2=[3,4]
 arr3=[0]*len(arr1)
 k=0
@@ -44,7 +44,7 @@ while i < len(arr1) and j < len(arr2):
     if k==len(arr1):
         break
 
-print(arr3)'''
+print(arr3)
 
 #Find the missing number in an array
 
@@ -54,12 +54,12 @@ print(arr3)'''
 #Problem Statement: Given an integer N and an array of size N-1 containing N-1 numbers between 1 to N. 
 #Find the number(between 1 to N), that is not present in the given array..
 
-arr=[1,2,3,4,5]
+'''arr=[1,2,3,4,5]
 n=5
 first_elemnet=arr[0]
 for i in range(first_elemnet,n+1):
     if i not in arr:
-        print(i)
+        print(i)'''
 
 
 #Count Maximum Consecutive One's in the array
