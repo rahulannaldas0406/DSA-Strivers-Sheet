59d248603dac1ee74db791198b02b232d39085df
Annaldas Rahul <rahulannaldas2006@gmail.com>
2026-02-23 22:30:08 +0530

    Update code

---
+++ Arrays/day13.py
@@ -0,0 +1,12 @@
+#Binary Search: Explained
+#Problem statement: You are given a sorted array of integers and a target, your task is to search for the target in the 
+# given array. Assume the given array does not contain any duplicate numbers.
+
+arr=[34,23,13,43]
+
+num=23
+for i in arr:
+    if i ==num:
+        print("true")
+        break
+    
