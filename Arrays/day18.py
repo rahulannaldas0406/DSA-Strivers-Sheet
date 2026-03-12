09f51a88d8f7153223faa15e83be82c120df2aea
Annaldas Rahul <rahulannaldas2006@gmail.com>
2026-03-12 22:38:40 +0530

    BS answers

--- Arrays/day18.py
+++ Arrays/day18.py
@@ -28,4 +28,36 @@ print(sqrt_poztv(25))'''
 
 #Nth Root of a Number using Binary Search
 
-#Problem Statement: Given two numbers N and M, find the Nth root of M. The nth root of a number M is defined as a number X when raised to the power N equals M. If the 'nth root is not an integer, return -1.
+#Problem Statement: Given two numbers N and M, find the Nth root of M. 
+# The nth root of a number M is defined as a number X when raised to the power N equals M. 
+# If the 'nth root is not an integer, return -1.
+
+# def findNroot(n,m):
+#     res=0
+#     a=False
+#     while a
+
+        
+#     return -1
+# print(findNroot(3,27))
+
+'''n=5
+m=32
+left=1
+right=27
+while left<=right:
+    mid=left+(right-left)//2
+    if mid**n==m:
+        print(mid)
+        break
+    elif mid**n>m:
+        right=mid-1
+    else:
+        left=mid+1
+else:
+    print("-1")'''
+
+
+
+
+    
