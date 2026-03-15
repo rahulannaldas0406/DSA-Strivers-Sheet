f0ae33016f091d64697965ce0b3e323275baabec
Annaldas Rahul <rahulannaldas2006@gmail.com>
2026-03-15 22:55:09 +0530

    BS 1D Array

--- Arrays/day18.py
+++ Arrays/day18.py
@@ -60,4 +60,50 @@ else:
 
 
 
+'''arr=[3, 6, 7, 11]
+h=8
+left=1
+right=max(arr)
+while left<=right:
+    sum_hr=0
+    mid=(left+right)//2
+    for i in arr:
+        hr=(i+mid-1)//mid
+        sum_hr+=hr
+    if sum_hr<=h:
+        right=mid-1
+    else:
+        left=mid+1
     
+print(left)'''
+
+arr=[7, 7, 7, 7, 13, 11, 12, 7]
+m=2
+k=3
+left=min(arr)
+right=max(arr)
+ans=-1
+while left<=right:
+    mid=(left+right)//2
+
+    b=0
+    count=0
+    
+    for i in arr:
+        if i<=mid:
+            count+=1
+            if count==k:
+                b+=1
+                count=0
+
+        else:
+            count=0
+
+    if b >= m:
+        ans = mid        # store answer
+        right = mid - 1  # try smaller day
+    else:
+        left = mid + 1
+
+
+print(ans)
