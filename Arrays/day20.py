019709d1da96d87c35ae5a26d6088110d1a340b1
Annaldas Rahul <rahulannaldas2006@gmail.com>
2026-03-18 22:43:27 +0530

    1D Arrays BS

---
+++ Arrays/day20.py
@@ -0,0 +1,6 @@
+#Capacity to Ship Packages within D Days
+
+'''Problem Statement: You are the owner of a Shipment company. You use conveyor belts to ship packages from one port to another. 
+The packages must be shipped within 'd' days. The weights of the packages are given in an array 'of weights'.
+ The packages are loaded on the conveyor belts every day in the same order as they appear in the array. The loaded weights must not exceed the maximum weight capacity of the ship. 
+ Find out the least-weight capacity so that you can ship all the packages within 'd' days .'''
