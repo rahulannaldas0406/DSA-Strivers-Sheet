2223d26f883b3082af3eb3167f5a2307a73f0af2
Annaldas Rahul <rahulannaldas2006@gmail.com>
2026-03-22 23:45:38 +0530

    BS 1D Arrays

--- Arrays/day17.py
+++ Arrays/day17.py
@@ -4,7 +4,7 @@
 #  Formally, if arr[i] is the peak element, arr[i - 1] < arr[i] and arr[i + 1] < arr[i]. Find the index(0-based) of a peak element in the array. 
 # If there are multiple peak numbers, return the index of any peak number.
 
-arr=[1,2,3,4,5,6,7,8,5,1]
+'''arr=[1,2,3,4,5,6,7,8,5,1]
 
 max=arr[0]
 ind=0
@@ -12,4 +12,7 @@ for i in range(len(arr)):
     if arr[i]>max:
         max=arr[i]
         ind=i
-print(ind)
+print(ind)'''
+
+#<----Completed 1D Arrays---->
+
