f0d1c98b81fd815062fe26029f8aaf3b3efa576a
Annaldas Rahul <rahulannaldas2006@gmail.com>
2026-03-30 23:03:49 +0530

    1D Arrays BS

--- Arrays/day20.py
+++ Arrays/day20.py
@@ -42,3 +42,58 @@ print(ans)'''
 Problem Statement: You are given an array 'arr' of size 'n' which denotes the position of stalls. You are also given an integer 'k' which denotes the number of aggressive cows.
 You are given the task of assigning stalls to 'k' cows such that the minimum distance between any two of them is the maximum possible. Find the maximum possible minimum distance.'''
 
+
+'''BS on Answer:
+1. Define search space
+2. Check feasibility (greedy)
+3. If possible → try bigger
+4. Else → reduce'''
+
+from numpy import sort
+
+
+def canWePlace(arr,dist,cows):
+    last=arr[0]
+    count_cows=1
+
+    for i in range(1,len(arr)):
+        if arr[i]-last>=dist:
+            count_cows+=1
+            last=arr[i]
+
+        if count_cows>=cows:
+            return True
+    else:
+        return False
+    
+def aggressiveCows(arr,k):
+    stalls=sort(arr) 
+    n=len(stalls)
+
+    low=1
+    high=stalls[n-1]-stalls[0]
+
+    while low<=high:
+        mid=(low+high)//2
+
+        if canWePlace(stalls,mid,k):
+            low=mid+1
+
+        else:
+            high=mid-1
+
+    return high
+
+print(aggressiveCows([0,3,4,7,10,9],4))
+
+
+#Same pattern used in:
+
+# Aggressive Cows 🐄
+# Koko Eating Bananas 🍌
+# Allocate Books 📚
+# Painters Partition 🎨
+# Ship Packages 🚢
+
+
+
