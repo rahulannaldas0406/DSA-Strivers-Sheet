fa58593b23c117e459c65d03c2657b81f7dfaec0
Annaldas Rahul <rahulannaldas2006@gmail.com>
2026-06-30 23:44:38 +0530

    Update code

--- Arrays/day21.py
+++ Arrays/day21.py
@@ -1,2 +1,54 @@
 #something gone wrong
 
+# N=int(input())
+# arr=list(map(int,input().split()))
+# res=0
+# for i in arr:
+#     res^=i
+
+# print(res)
+
+arr=[2,4,9,3]
+res=[0]*len(arr)
+
+window_sum=0
+# k=2
+# for i in range(1,k+1):
+#     window_sum+=arr[i]
+
+# res[0]=window_sum
+
+# for i in range(k+1,len(arr)+k):
+
+#     window_sum=window_sum-arr[(i-k)%len(arr)]+arr[i%len(arr)]
+#     res[i-k]=window_sum
+# print(res)
+# k=-2
+# for i in range(1,abs(k)+1):
+#     window_sum+=arr[(0-i)%4]
+# print(window_sum)
+
+# for i in range(1,len(arr)-1):
+#     window_sum=window_sum-arr[(i+k-1)%len(arr)]+arr[(i-1)%len(arr)]
+#     res[i]=window_sum
+# 0 1 2 3
+# 0
+# print((0-1)%4)#3
+# print((0-2)%4)#2 #rem
+# 1
+# print((0-2)%4) #2
+# print() to get rem what should i do 
+# print((1+2-1)%4)
+# print((1-1)%4)
+
+
+# s="abcabc"
+# for i in range(3):
+#     window_str=
+
+# arr = [8, 2, 4, 5, 3, 7, 1]
+# n=len(arr)+1
+# expsum=n*(n+1)//2
+# print(expsum)
+# print(sum(arr))
+
